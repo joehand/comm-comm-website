d2cd4e4dcd6855c50a66eebe3d5196429cfdd133
Joe Hand <joe@hand.email>
2019-02-13 22:39:20 -0800

    better wording on 404

--- views/404.js
+++ views/404.js
@@ -8,7 +8,7 @@ function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   return html`
     <body class="sans-serif pa3">
-      <h1>Route not found.</h1>
+      <h1>Page not found.</h1>
       <a class="pt2" href="/">Back to main.</a>
     </body>
   `
